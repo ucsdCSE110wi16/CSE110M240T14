34e12f948a0e3147a3b4954b3ce72c6c50fdcda9
diqian <qiandi.eric@gmail.com>
2016-02-01 12:52:14 -0800

    Toilet Wall Intial Code

---
+++ Flushr App/ToiletWall/Code/ToiletWall/app/src/main/java/com/visiondq/www/toiletwall/MainActivity.java
@@ -0,0 +1,66 @@
+package com.visiondq.www.toiletwall;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawingView drawView;
+
+    private ImageButton currPaint, drawBtn, eraseBtn;
+
+    boolean colorPalletisOpen = false;
+    /*
+
+    public void changeColor(View view){
+
+        if(colorPalletisOpen == false) {
+            LinearLayout layout = (LinearLayout) findViewById(R.id.colorPalleteLayout);
+            layout.setVisibility(View.VISIBLE);
+        }
+
+    }
+*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawView = (DrawingView)findViewById(R.id.drawing);
+
+        eraseBtn = (ImageButton)findViewById(R.id.eraser);
+
+       // eraseBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
