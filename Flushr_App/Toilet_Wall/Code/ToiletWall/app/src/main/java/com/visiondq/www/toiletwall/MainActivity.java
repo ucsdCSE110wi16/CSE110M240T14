a6327c8963d63c8f76dfc6f89204a51c9a260033
diqian <qiandi.eric@gmail.com>
2016-02-02 17:40:54 -0800

    New Toilet Wall

--- Flushr_App/Toilet_Wall/Code/ToiletWall/app/src/main/java/com/visiondq/www/toiletwall/MainActivity.java
+++ Flushr_App/Toilet_Wall/Code/ToiletWall/app/src/main/java/com/visiondq/www/toiletwall/MainActivity.java
@@ -1,5 +1,6 @@
 package com.visiondq.www.toiletwall;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,27 +16,70 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawingView drawView;
 
-    private ImageButton currPaint, drawBtn, eraseBtn;
+    private ImageButton currPaint, drawBtn, eraseBtn, redBtn, yellowBtn;
+    private ImageButton darkblueBtn, greenBtn, grayBtn,orangeBtn;
 
     boolean colorPalletisOpen = false;
-    /*
+
+
+    public void paintClicked(View view){
+        //use chosen color
+        if(view!=currPaint){
+            //update color
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+            currPaint = imgView;
+        }
+    }
+
 
     public void changeColor(View view){
 
         if(colorPalletisOpen == false) {
-            LinearLayout layout = (LinearLayout) findViewById(R.id.colorPalleteLayout);
-            layout.setVisibility(View.VISIBLE);
+            yellowBtn = (ImageButton)findViewById(R.id.yellowColorButton);
+            yellowBtn.setVisibility(View.VISIBLE);
+            redBtn = (ImageButton)findViewById(R.id.redColorButton);
+            redBtn.setVisibility(View.VISIBLE);
+            greenBtn = (ImageButton)findViewById(R.id.greenColorButton);
+            greenBtn.setVisibility(View.VISIBLE);
+            grayBtn = (ImageButton)findViewById(R.id.greyColorButton);
+            grayBtn.setVisibility(View.VISIBLE);
+            darkblueBtn = (ImageButton)findViewById(R.id.darkblueColorButton);
+            darkblueBtn.setVisibility(View.VISIBLE);
+            orangeBtn = (ImageButton)findViewById(R.id.orangeColorButton);
+            orangeBtn.setVisibility(View.VISIBLE);
+            colorPalletisOpen = true;
+            //LinearLayout layout = (LinearLayout) findViewById(R.id.colorPalleteLayout);
+            //layout.setVisibility(View.VISIBLE);
+        }else{
+            yellowBtn = (ImageButton)findViewById(R.id.yellowColorButton);
+            yellowBtn.setVisibility(View.INVISIBLE);
+            redBtn = (ImageButton)findViewById(R.id.redColorButton);
+            redBtn.setVisibility(View.INVISIBLE);
+            greenBtn = (ImageButton)findViewById(R.id.greenColorButton);
+            greenBtn.setVisibility(View.INVISIBLE);
+            grayBtn = (ImageButton)findViewById(R.id.greyColorButton);
+            grayBtn.setVisibility(View.INVISIBLE);
+            darkblueBtn = (ImageButton)findViewById(R.id.darkblueColorButton);
+            darkblueBtn.setVisibility(View.INVISIBLE);
+            orangeBtn = (ImageButton)findViewById(R.id.orangeColorButton);
+            orangeBtn.setVisibility(View.INVISIBLE);
+            colorPalletisOpen = false;
+            //
         }
-
     }
-*/
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         drawView = (DrawingView)findViewById(R.id.drawing);
-
+        currPaint = (ImageButton)findViewById(R.id.darkblueColorButton);
         eraseBtn = (ImageButton)findViewById(R.id.eraser);
 
        // eraseBtn.setOnClickListener(this);
